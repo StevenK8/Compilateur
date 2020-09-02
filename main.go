a2b4926879abc796af5a7215c6ef5a3006c3eb99
STEVEN KERAUTRET <steven928@hotmail.fr>
2020-09-02 12:58:13 +0200

    add isfirstletter

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"io/ioutil"
 )
 
+type token struct {
+	dataType     string
+	valeurString string
+	valeurInt    int
+	nbLigne      int
+}
+
 func main() {
 	operators := [6]string{"+", "-", "*", "/", "%", "^"}
 	ident := [4]string{"if", "for", "while", "else"}
@@ -14,19 +21,41 @@ func main() {
 		fmt.Println("File reading error", err)
 		return
 	}
+
 	for charPos := 0; charPos < len(data); charPos++ {
-		fmt.Println(string(data[charPos]))
+
+		letter := string(data[charPos])
+		fmt.Println(letter)
+
 		for _, op := range operators {
-			if op == string(data[charPos]) {
+			if isFirstLetter(letter, op) {
 				fmt.Println("operator")
 			}
+
 		}
 
-		for _, op := range ident {
-			if op == string(data[charPos]) {
+		for _, id := range ident {
+			if isFirstLetter(letter, id) {
 				fmt.Println("ident")
 			}
 		}
 	}
 
+}
+
+func isFirstLetter(char string, word string) bool {
+	if char == string(word[0]) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
 }
