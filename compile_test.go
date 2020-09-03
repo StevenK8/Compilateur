f6a89875cc9e8f441530159928e40d67b2a60062
STEVEN KERAUTRET <steven928@hotmail.fr>
2020-09-03 13:10:18 +0200

    add lexer

---
+++ compile_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestOperator(t *testing.T) {
+	texte := "3 * 2"
+	lexer(texte)
+}
--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
+)
+
+type LeaveType string
+
+const (
+	AnnualLeave LeaveType = "AnnualLeave"
+	Sick                  = "Sick"
+	BankHoliday           = "BankHoliday"
+	Other                 = "Other"
 )
 
 type token struct {
@@ -13,37 +23,84 @@ type token struct {
 }
 
 func main() {
-	operators := [6]string{"+", "-", "*", "/", "%", "^"}
-	ident := [4]string{"if", "for", "while", "else"}
 
-	data, err := ioutil.ReadFile("test.go")
+	data, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
 
-	for charPos := 0; charPos < len(data); charPos++ {
-
-		letter := string(data[charPos])
-		fmt.Println(letter)
-
-		for _, op := range operators {
-			if isFirstLetter(letter, op) {
-				fmt.Println("operator")
-			}
-
-		}
-
-		for _, id := range ident {
-			if isFirstLetter(letter, id) {
-				fmt.Println("ident")
-			}
-		}
+	tokenTab := lexer(data)
+	for _, a := range tokenTab {
+		println(string(a.valeurString))
 	}
 
 }
 
-func isFirstLetter(char string, word string) bool {
+func lexer(data []byte) []token {
+
+	var isLetter bool
+	var isOperator bool
+	var isNumber bool
+	var currentWord string
+	var tokenTab []token
+
+	for charPos := 0; charPos < len(data); charPos++ {
+
+		currentChar := string(data[charPos])
+
+		matched, err := regexp.MatchString(`[a-zA-Z]`, currentChar)
+		if err != nil {
+			println(err)
+		}
+
+		if matched && (isLetter || len(currentWord) == 0) {
+			currentWord += currentChar
+			isLetter = true
+		} else if isLetter && !matched {
+			tokenTab = append(tokenTab, token{"lettre", currentWord, 0, charPos})
+			currentWord = ""
+			isLetter = false
+		}
+
+		matched, err = regexp.MatchString(`[0-9]`, currentChar)
+		if err != nil {
+			println(err)
+		}
+
+		if matched && (isNumber || len(currentWord) == 0) {
+			currentWord += currentChar
+			isLetter = false
+			isNumber = true
+			isOperator = false
+		} else if isNumber && !matched {
+			tokenTab = append(tokenTab, token{"nombre", currentWord, 0, charPos})
+			currentWord = ""
+			isNumber = false
+		}
+
+		matched, err = regexp.MatchString(`[(+*;=]`, currentChar)
+		if err != nil {
+			println(err)
+		}
+
+		if matched && (isOperator || len(currentWord) == 0) {
+			currentWord += currentChar
+			isLetter = false
+			isNumber = false
+			isOperator = true
+		} else if isOperator && !matched {
+			tokenTab = append(tokenTab, token{"ope", currentWord, 0, charPos})
+			currentWord = ""
+			isOperator = false
+		}
+
+		// fmt.Println(currentChar)
+	}
+	return tokenTab
+}
+
+func isFirstcurrentChar(char string, word string) bool {
 	if char == string(word[0]) {
 		return true
 	} else {
@@ -56,6 +113,22 @@ func contains(arr []string, str string) bool {
 		if a == str {
 			return true
 		}
+	}
+	return false
+}
+
+func isOperator(op string) bool {
+	operators := []string{"+", "-", "*", "/", "%", "^"}
+	if contains(operators, op) {
+		return true
+	}
+	return false
+}
+
+func isIdent(id string) bool {
+	ident := []string{"if", "for", "while", "else"}
+	if contains(ident, id) {
+		return true
 	}
 	return false
 }
