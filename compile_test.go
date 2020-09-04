c04abf45955fe45ba491564f0991aa0f7dc293f2
STEVEN KERAUTRET <steven928@hotmail.fr>
2020-09-04 10:21:13 +0200

    add lexer test

--- compile_test.go
+++ compile_test.go
@@ -4,5 +4,12 @@ import "testing"
 
 func TestOperator(t *testing.T) {
 	texte := "3 * 2"
-	lexer(texte)
+	tokenTab := lexer([]byte(texte))
+	if string(tokenTab[0].valeurString) != "3" {
+		t.Errorf("Erreur")
+	}
+	if string(tokenTab[1].valeurString) != "*" {
+		t.Errorf("Erreur")
+	}
+
 }
